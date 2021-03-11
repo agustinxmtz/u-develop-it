7d08174078123e37833e7542868564aae9e05bdc
agustinxmtz <70080354+agustinxmtz@users.noreply.github.com>
2021-03-11 16:16:20 -0600

    establish connection with the SQLite Database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const sqlite3 = require('sqlite3').verbose();
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,18 +9,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the election database.');
+});
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
     });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
 });
 
